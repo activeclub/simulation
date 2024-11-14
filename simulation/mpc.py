f5932584e28c728a3d16121aae9b68f91ed621ca
Naoto Nishizaka <nszk.nao@gmail.com>
2024-11-14 23:57:09 +0900

    Add humanoid.xml model and mpc simulation script

---
+++ simulation/mpc.py
@@ -0,0 +1,19 @@
+import os
+import mujoco
+from mujoco_mpc import agent as agent_lib
+import pathlib
+
+model_path = (
+    f"{os.path.dirname(__file__)}/../mujoco_mpc/build/mjpc/tasks/humanoid/walk/task.xml"
+)
+model = mujoco.MjModel.from_xml_path(str(model_path))
+
+with agent_lib.Agent(
+    server_binary_path=pathlib.Path(agent_lib.__file__).parent
+    / "mjpc"
+    / "ui_agent_server",
+    task_id="Humanoid Walk",
+    model=model,
+) as agent:
+    while True:
+        pass
