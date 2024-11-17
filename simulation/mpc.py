263b2dd0265ee55afbe5f4a7373ff1938d13ecbf
Naoto Nishizaka <nszk.nao@gmail.com>
2024-11-17 19:02:21 +0900

    Update humanoid and cartpole tasks in MPC agent initialization

--- simulation/mpc.py
+++ simulation/mpc.py
@@ -5,16 +5,21 @@ import mujoco
 
 from mujoco_mpc import agent as agent_lib
 
-model_path = (
-    f"{os.path.dirname(__file__)}/../mujoco_mpc/build/mjpc/tasks/humanoid/walk/task.xml"
-)
+mujoco_mpc_base_path = f"{os.path.dirname(__file__)}/../mujoco_mpc"
+
+# model_path = f"{mujoco_mpc_base_path}/build/mjpc/tasks/humanoid/walk/task.xml"
+# task_id = "Humanoid Walk"
+
+model_path = f"{mujoco_mpc_base_path}/build/mjpc/tasks/cartpole/task.xml"
+task_id = "Cartpole"
+
 model = mujoco.MjModel.from_xml_path(str(model_path))
 
 with agent_lib.Agent(
     server_binary_path=pathlib.Path(agent_lib.__file__).parent
     / "mjpc"
     / "ui_agent_server",
-    task_id="Humanoid Walk",
+    task_id=task_id,
     model=model,
 ) as agent:
     while True:
